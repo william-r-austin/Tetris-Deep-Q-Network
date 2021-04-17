2f2208f79b0279f0d5d4875a61c6d1980ab9b622
William Austin <william.r.austin@gmail.com>
2021-04-17 12:10:24 -0400

    Initial version of Tetris DQN with simulator.

---
+++ src/cs747/main/RunRandomTetrisSim.py
@@ -0,0 +1,31 @@
+'''
+Created on Apr 11, 2021
+
+@author: William
+'''
+from cs747.sim.tetris import Tetris
+import random
+
+if __name__ == '__main__':
+    possible_actions = ["MOVE_LEFT", "MOVE_RIGHT", "MOVE_DOWN", "DROP", "ROTATE_CLOCKWISE", "ROTATE_COUNTERCLOCKWISE"]
+    tetrisSim = Tetris()
+    game_number = 1
+    
+    while game_number <= 1000:
+        print("\nStarting new Tetris Game #" + str(game_number))
+        
+        action_count = 0
+        game_over = False
+        
+        while not game_over:
+            next_action = random.choice(possible_actions)
+            result_map = tetrisSim.doAction(next_action)
+            action_count += 1
+            #print("Action was:" + next_action + ". Result was " + str(result_map))
+            game_over = result_map["gameover"]
+        
+        print("Finished Game #" + str(game_number) + ". Total actions = " + str(action_count))
+        game_number += 1
+        tetrisSim.reset()
+        
+    print("Done playing tetris!!")
